c7e2cd91dee4b215680742da4ee0e99c8ddfbc94
takumi <y.takumi4@gmail.com>
2022-05-13 15:28:14 +0900

    heroku postgre接続スクリプトを作成

---
+++ connect_heroku_postgreSQL.py
@@ -0,0 +1,27 @@
+import sys,os,logging,psycopg2
+
+host = os.environ['RDS_HOST']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+dbname = os.environ['DB_NAME']
+port = os.environ['PORT']
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+try:
+    conn = psycopg2.connect(dbname=dbname,user=username,password=password,host=host,port=port)
+
+except psycopg2.MySQLError as e:
+    logging.error('ERROR: Unexpected error: Could not connect to MySQL instance.')
+    logging.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+def handler(event, context):
+    with conn.cursor() as cur:
+        cur.execute("select * from ShiftManagementApp_shift;")
+        conn.commit()
+        logging.debug(cur.fetchall())
+    conn.commit()
+    conn.close()
