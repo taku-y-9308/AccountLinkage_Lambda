dfb9a222b1ecb1f67a8595b746e81335fa6bde0d
takumi <y.takumi4@gmail.com>
2022-05-13 15:56:21 +0900

    テーブル名をダブルクオーテーションで囲む

--- connect_heroku_postgreSQL.py
+++ connect_heroku_postgreSQL.py
@@ -13,14 +13,14 @@ try:
     conn = psycopg2.connect(dbname=dbname,user=username,password=password,host=host,port=port)
 
 except psycopg2.OperationalError as e:
-    logging.error('ERROR: Unexpected error: Could not connect to MySQL instance.')
+    logging.error('ERROR: Unexpected error: Could not connect to PostgreSQL instance.')
     logging.error(e)
     sys.exit()
 
 logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 def handler(event, context):
     with conn.cursor() as cur:
-        cur.execute("select * from ShiftManagementApp_shift;")
+        cur.execute('select * from "ShiftManagementApp_shift";')
         conn.commit()
         logging.debug(cur.fetchall())
     conn.commit()
