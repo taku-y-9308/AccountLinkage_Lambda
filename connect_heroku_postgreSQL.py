dca9bbe7c62984269acf583bf813ae367e284b7c
takumi <y.takumi4@gmail.com>
2022-05-14 15:29:32 +0900

    リプライメッセージを追加

--- connect_heroku_postgreSQL.py
+++ connect_heroku_postgreSQL.py
@@ -1,4 +1,15 @@
+from email import message
 import sys,os,logging,psycopg2
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+LINE_CHANNEL_ACCESS_TOKEN   = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+LINE_CHANNEL_SECRET         = os.environ['LINE_CHANNEL_SECRET']
+LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+LINE_HANDLER = WebhookHandler(LINE_CHANNEL_SECRET)
 
 host = os.environ['HOST']
 username = os.environ['USERNAME']
@@ -19,11 +30,23 @@ except psycopg2.OperationalError as e:
 
 logger.info("SUCCESS: Connection to heroku PostgreSQL succeeded")
 def handler(event, context):
+    logger.info(event)
+    signature = event["headers"]["x-line-signature"]
+    body = event["body"]
     with conn.cursor() as cur:
         cur.execute('select date,begin,finish,user_id,username from "ShiftManagementApp_shift" inner join "ShiftManagementApp_user" on "ShiftManagementApp_shift".user_id = "ShiftManagementApp_user".id;')
         conn.commit()
         results = cur.fetchall()
+        message = ""
         for result in results:
             print(f"date:{result[0]} start:{result[1]} end:{result[2]} user_id:{result[3]} username:{result[4]}")
+            message += f"date:{result[0]} start:{result[1]} end:{result[2]}"
     conn.commit()
     conn.close()
+
+    @LINE_HANDLER.add(MessageEvent, message=TextMessage)
+    def on_message(line_event):
+        LINE_BOT_API.reply_message(line_event.reply_token, TextSendMessage(message))
+
+    LINE_HANDLER.handle(body, signature)
+    return 0
